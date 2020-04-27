91a1bc306b87cee8cbd7e89a5724281cc0f5bf69
heringnatalia <hering.natalia@gmail.com>
2020-04-27 17:13:16 +0300

    Restructure folders

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../css/App.css';
 import Button from '@material-ui/core/Button';
 
 function App() {
