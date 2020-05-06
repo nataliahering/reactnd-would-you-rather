32d87a0d738ade2a5e01996330929bf398ede7d5
heringnatalia <hering.natalia@gmail.com>
2020-05-06 23:07:48 +0300

    Get rid of redundant List in UserScore component

--- src/components/UserScore.js
+++ src/components/UserScore.js
@@ -1,10 +1,9 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
-import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
@@ -12,50 +11,50 @@ const useStyles = makeStyles((theme) => ({
     width: '100%',
     maxWidth: '36ch',
     backgroundColor: theme.palette.background.paper,
-  }
+  },
+  block: {
+    display: 'block',
+  },
 }));
 
 export default function UserScore(props) {
   const classes = useStyles();
+
   return (
     <ListItem alignItems="flex-start">
       <ListItemAvatar>
-        <Avatar alt="Remy Sharp" src={props.userScore.avatar} />
+        <Avatar src={props.userScore.avatar} />
       </ListItemAvatar>
       <ListItemText
         primary={props.userScore.name}
         secondary={
-            <List aria-label="secondary mailbox folders">
-              <ListItem>
-                <Typography
-                  variant="body2"
-                  className={classes.inline}
-                  color="textPrimary"
-                >
-                  Answered Questions: {props.userScore.numberOfAnswers}
-                </Typography>
-              </ListItem>
-              <ListItem>
-                <Typography
-                  variant="body2"
-                  className={classes.inline}
-                  color="textPrimary"
-                >
-                  Created Questions: {props.userScore.numberOfQuestions}
-                </Typography>
-              </ListItem>
-              <ListItem>
-                <Typography
-                  variant="body1"
-                  className={classes.inline}
-                  color="textPrimary"
-                >
-                  SCORE: {props.userScore.numberOfAnswers + props.userScore.numberOfQuestions}
-                </Typography>
-              </ListItem>
-            </List>
+          <Fragment>
+            <Typography
+              component="span"
+              variant="body2"
+              className={classes.block}
+              color="textPrimary"
+            >
+              Answered Questions: {props.userScore.numberOfAnswers}
+            </Typography>
+            <Typography
+              component="span"
+              variant="body2"
+              className={classes.block}
+              color="textPrimary"
+            >
+              Created Questions: {props.userScore.numberOfQuestions}
+            </Typography>
+            <Typography
+              variant="body1"
+              className={classes.block}
+              color="textPrimary"
+            >
+              Score: {props.userScore.numberOfAnswers + props.userScore.numberOfQuestions}
+            </Typography>
+          </Fragment>
         }
       />
     </ListItem>
-    );
+  );
 }
